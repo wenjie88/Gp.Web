5ced7e13c86e05453567933b03ee04dbd82ff5ae
wenjie88 <350668770@qq.com>
2017-12-23 17:56:15 +0800

    最新

--- Gp.Web/Controllers/InfoController.cs
+++ Gp.Web/Controllers/InfoController.cs
@@ -30,231 +30,197 @@ namespace Gp.Web.Controllers
         public ActionResult GetInfo(string StartTime, string EndTime)
         {
             //
-            List<TradeInfo> list = new List<TradeInfo>();
-            if (String.IsNullOrEmpty(StartTime) || String.IsNullOrEmpty(EndTime))
-            {
-                list = _TradeInfoDao.GetList_ByUserId(AdminManager.GetUser().UserId).ToList();
-            }
-            else
-            {
-                list = _TradeInfoDao.GetList_ByUserIdAndDealDate(AdminManager.GetUser().UserId, StartTime + " 00:00:00", EndTime + " 23:59:59").ToList();
-            }
+            List<TradeInfo> list = _TradeInfoDao.GetList_ByUserId(AdminManager.GetUser().UserId).ToList();
 
 
             //无论前端选择日期范围从哪里开始到哪里，  这里必须从用户第一次买股开始计算,  最后返回用户选择日期范围的数据就行了。 
 
+            //上个月还没结算的股票缓存，到下个月拿出来看是否可以结算
+            Dictionary<string, List<TradeInfo>> d = new Dictionary<string, List<TradeInfo>>();
+
             //统计每个月收益
-            var g = list.GroupBy(x => "" + x.DealDate.Year + x.DealDate.Month).Select(group =>
+            var month_group = list.Where(x => !String.IsNullOrEmpty(x.Code)).GroupBy(x => "" + x.DealDate.Year + "年" + x.DealDate.Month.ToString().PadLeft(2, '0') + "月");
+            List<MonthSummary> MonthList = new List<MonthSummary>();
+            foreach (var group in month_group)
             {
-                //这个月的股票分组
+                //这个月的所有股票买卖交割单
+                var j_list = group.OrderBy(x => x.DealDate);
 
-                //根据代码分组股票
-                group.GroupBy(x => x.Code).Select(codeGroup =>
+                //这个月的所有股票结算 (可以结算的股票， 不包含没完全卖出的股票)
+                List<Summary> AllSummary = new List<Summary>();
+
+
+                ////一个月当中，可能会对同一个股票，多次完整结算。即多次完整买卖(今日买入100股，明天卖出100股，然后过10天又重新买100股，后天又卖出100股) 
+                //需要结算的股票缓存, 结算完就会移除
+                foreach (TradeInfo item in j_list)
                 {
-                    //这里就是同一只股票的所有交割单 
-
-                    //遍历， 统计这个股票在这个月中 买入卖出总数。
-                    int buyCount = 0;
-                    int sellerCount = 0;
-                    double shouyi = 0.00;
-                    foreach (TradeInfo item in codeGroup)
+                    if (d.ContainsKey(item.Code))
                     {
-                        switch (item.Operation)
+                        d[item.Code].Add(item);
+
+                        //判断是否可以结算了, 一旦买入数量等于卖出数量， 那就等同已经结算(完成一个完整的买入卖出) ,那就可以统计收益
+                        Summary summary = IsCanJieSuan(d[item.Code]);
+                        //可以结算
+                        if (summary != null)
                         {
-                            case "证券买入":
-                                buyCount += item.DealCount;
-                                break;
-                            case "证券卖出":
-                                sellerCount += item.DealCount;
-                                break;
+                            AllSummary.Add(summary);
+
+                            //移除缓存
+                            d.Remove(item.Code);
                         }
 
-                        //一旦买入数量等于卖出数量， 那就等同已经结算(完成一个完整的买入卖出) ,那就可以统计收益
-                        //一个月当中，可能会对同一个股票，多次完整结算。即多次完整买卖(今日买入100股，明天卖出100股，然后过10天又重新买100股，后天又卖出100股) 
-                        if (buyCount == sellerCount)
-                        {
-                            shouyi = codeGroup.Sum(x => x.HappenAmount);
-                        }
                     }
-
-
-                    //如果不等于，  那就证明没全部卖出， 那就不结算。
-
-
-                    return new
+                    else
                     {
-                        Code = codeGroup.First().Code,
-                        Name = codeGroup.First().Name,
-                        Shouyi = shouyi
-                    };
+                        d.Add(item.Code, new List<TradeInfo>() { item });
+                    }
+                }
+
+                MonthList.Add(new MonthSummary
+                {
+                    Date = group.Key,
+                    MonthShouYi = Math.Round(AllSummary.Sum(x => x.TotalHappenAmount), 2, MidpointRounding.AwayFromZero),
+                    MonthTradeCount = AllSummary.Count,
+                    DataList = AllSummary
                 });
 
 
+            }
 
-                double _TotalDealAmount = 0;    //总成交金额
-                double _TotalHappenAmount = 0;  //总收益
-                double _TotalPoundage = 0;      //总手续费
-                double _TotalStamp_Tax = 0;     //总印花税
-                double _TotalOther_Free = 0;    //总其他杂费( 过户费 )
-                int _TotalCount = 0;            //总成交次数
-                int _TotalSellCount = 0;             //卖出股数
-                int _TotalBuyCount = 0;              //买入股数
-
-                foreach (TradeInfo item in group)
-                {
-                    _TotalDealAmount += item.DealAmount;
-                    _TotalHappenAmount += item.HappenAmount;
-                    _TotalPoundage += item.Poundage;
-                    _TotalStamp_Tax += item.Stamp_Tax;
-                    _TotalOther_Free += item.Other_Free;
-                    _TotalCount += 1;
-                    switch (item.Operation)
-                    {
-                        case "证券买入":
-                            _TotalBuyCount += item.DealCount;
-                            break;
-                        case "证券卖出":
-                            _TotalSellCount += item.DealCount;
-                            break;
-                        default:
-
-                            break;
-                    }
-                }
-
-                return new
-                {
-                    date = group.Key,
-                    TotalDealAmount = _TotalDealAmount.ToString("0.00"),    //总成交金额
-                    TotalHappenAmount = _TotalHappenAmount.ToString("0.00"),  //总收益
-                    TotalPoundage = _TotalPoundage.ToString("0.00"),      //总手续费
-                    TotalStamp_Tax = _TotalStamp_Tax.ToString("0.00"),    //总印花税
-                    TotalOther_Free = _TotalOther_Free.ToString("0.00"),    //总其他杂费( 过户费 )
-                    TotalCount = _TotalCount,            //总成交次数
-                    TotalSellCount = _TotalSellCount,              //卖出股数
-                    TotalBuyCount = _TotalBuyCount,                //买入股数
-                    Data = group.ToList()
-                };
-            });
-
-
-
-
-
-
-
-
-
-            //int y_to_z_count = 0; //银行转证券次数
-            //double y_to_z_total = 0; //银行转证券总金额
-
-            //int z_to_y_count = 0; //证券转银行次数
-            //double z_to_y_total = 0; //证券转银行总金额
-
-            //代码分组
-            var code_group = list.GroupBy(x => x.Code).Select(group =>
+            //如果有选择日期范围， 则返回日期范围的数据
+            if (!(String.IsNullOrEmpty(StartTime) && String.IsNullOrEmpty(EndTime)))
             {
-                double _TotalDealAmount = 0;    //总成交金额
-                double _TotalHappenAmount = 0;  //总收益
-                double _TotalPoundage = 0;      //总手续费
-                double _TotalStamp_Tax = 0;     //总印花税
-                double _TotalOther_Free = 0;    //总其他杂费( 过户费 )
-                int _TotalCount = 0;            //总成交次数
-                int _TotalSellCount = 0;             //卖出股数
-                int _TotalBuyCount = 0;              //买入股数
+                DateTime start = DateTime.ParseExact(StartTime, "yyyy-MM", CultureInfo.InvariantCulture);
+                DateTime end = DateTime.ParseExact(EndTime, "yyyy-MM", CultureInfo.InvariantCulture);
 
-                foreach (TradeInfo item in group)
+                MonthList = MonthList.Where(x =>
                 {
-                    _TotalDealAmount += item.DealAmount;
-                    _TotalHappenAmount += item.HappenAmount;
-                    _TotalPoundage += item.Poundage;
-                    _TotalStamp_Tax += item.Stamp_Tax;
-                    _TotalOther_Free += item.Other_Free;
-                    _TotalCount += 1;
-                    switch (item.Operation)
-                    {
-                        case "证券买入":
-                            _TotalBuyCount += item.DealCount;
-                            break;
-                        case "证券卖出":
-                            _TotalSellCount += item.DealCount;
-                            break;
-                        default:
-
-                            break;
-                    }
-                }
-
-                return new
-                {
-                    Code = group.Key,
-                    Name = group.First().Name,
-                    TotalDealAmount = _TotalDealAmount.ToString("0.00"),    //总成交金额
-                    TotalHappenAmount = _TotalHappenAmount.ToString("0.00"),  //总收益
-                    TotalPoundage = _TotalPoundage.ToString("0.00"),      //总手续费
-                    TotalStamp_Tax = _TotalStamp_Tax.ToString("0.00"),    //总印花税
-                    TotalOther_Free = _TotalOther_Free.ToString("0.00"),    //总其他杂费( 过户费 )
-                    TotalCount = _TotalCount,            //总成交次数
-                    TotalSellCount = _TotalSellCount,              //卖出股数
-                    TotalBuyCount = _TotalBuyCount,                //买入股数
-                    Data = group.ToList()
-                };
-            });
-
-            //foreach (var group in code_group)
-            //{
-            //    foreach (TradeInfo item in list)
-            //    {
-            //        //非正常交易的
-            //        if (String.IsNullOrEmpty(item.Code))
-            //        {
-            //            if (item.Operation == "银行转证券")
-            //            {
-            //                y_to_z_count += 1;
-            //                y_to_z_total += item.HappenAmount;
-            //            }
-            //            else if (item.Operation == "证券转银行")
-            //            {
-            //                z_to_y_count += 1;
-            //                z_to_y_total += item.HappenAmount;
-            //            }
-            //        }
-            //        //正常交易的
-            //        else
-            //        {
-            //            _TotalDealAmount += item.DealAmount;
-            //            _TotalHappenAmount += item.HappenAmount;
-            //            _TotalPoundage += item.Poundage;
-            //            _TotalStamp_Tax += item.Stamp_Tax;
-            //            _TotalOther_Free += item.Other_Free;
-            //            _TotalCount += 1;
-            //        }
-
-            //    }
-            //}
-
-            //var resData = new
-            //{
-            //    Count_y_to_z = y_to_z_count, //银行转证券次数
-            //    Total_y_to_z = y_to_z_total.ToString("0.00"), //银行转证券总金额
-            //    Count_z_to_y = z_to_y_count, //证券转银行次数
-            //    Total_z_to_y = z_to_y_total.ToString("0.00"), //证券转银行总金额
-            //    TotalDealAmount = _TotalDealAmount.ToString("0.00"),    //总成交金额
-            //    TotalHappenAmount = _TotalHappenAmount.ToString("0.00"),  //总收益
-            //    TotalPoundage = _TotalPoundage.ToString("0.00"),      //总手续费
-            //    TotalStamp_Tax = _TotalStamp_Tax.ToString("0.00"),    //总印花税
-            //    TotalOther_Free = _TotalOther_Free.ToString("0.00"),    //总其他杂费( 过户费 )
-            //    TotalCount = _TotalCount            //总成交次数
-            //};
+                    var m = DateTime.ParseExact(x.Date, "yyyy年MM月", CultureInfo.InvariantCulture);
+                    return m >= start && m <= end;
+                }).ToList();
+            }
 
 
-            return Content(JSONHelper.SerializeObject(new { status = "ok", msg = "ok", result = code_group }));
+
+
+            var allSummaryList = MonthList.SelectMany(x => x.DataList);
+
+            return Content(JSONHelper.SerializeObject(new
+            {
+                status = "ok",
+                msg = "ok",
+                totalSouyi = Math.Round(MonthList.Sum(x => x.MonthShouYi), 2, MidpointRounding.AwayFromZero),
+                totalDeal = MonthList.Sum(x => x.MonthTradeCount),
+                MinRang = allSummaryList.OrderBy(x => x.TotalHappenAmount).Take(5),
+                MaxRang = allSummaryList.OrderByDescending(x => x.TotalHappenAmount).Take(5),
+                SigGpMaxRang = allSummaryList.GroupBy(x=>x.Code).Select(x=>x.Sum(y=>y.TotalHappenAmount)),
+                SuccessRate = Math.Round(allSummaryList.Where(x => x.TotalHappenAmount > 0).Count() * 1.0 / allSummaryList.Count(), 4, MidpointRounding.AwayFromZero),
+                result = MonthList,
+                d = d
+            }));
 
 
         }
 
 
 
+        private Summary IsCanJieSuan(List<TradeInfo> tradelist)
+        {
+            var AllDealCount = tradelist.Sum(x =>
+            {
+                if (x.Operation == "证券买入")
+                    return x.DealCount;
+                else if (x.Operation == "证券卖出")
+                    return -(x.DealCount);
+                else
+                    return 0;
+            });
+
+
+            //可以结算
+            if (AllDealCount == 0)
+            {
+                //开始结算
+                Summary summary = new Summary();
+
+                foreach (TradeInfo trade in tradelist)
+                {
+                    summary.TotalDealAmount += trade.DealAmount;
+                    summary.TotalHappenAmount += trade.HappenAmount;
+                    summary.TotalPoundage += trade.Poundage;
+                    summary.TotalStampTax += trade.Stamp_Tax;
+                    summary.TotalOtherFree += trade.Other_Free;
+                    summary.TotalCount += 1;
+                    switch (trade.Operation)
+                    {
+                        case "证券买入":
+                            summary.TotalBuyCount += trade.DealCount;
+                            break;
+                        case "证券卖出":
+                            summary.TotalSellCount += trade.DealCount;
+                            break;
+                        default:
+
+                            break;
+                    }
+                }
+                summary.Code = tradelist.First().Code;
+                summary.Name = tradelist.First().Name;
+                summary.JiaoGeList = tradelist;
+
+                summary.TotalDealAmount = Math.Round(summary.TotalDealAmount, 2, MidpointRounding.AwayFromZero);
+                summary.TotalHappenAmount = Math.Round(summary.TotalHappenAmount, 2, MidpointRounding.AwayFromZero);
+                summary.TotalPoundage = Math.Round(summary.TotalPoundage, 2, MidpointRounding.AwayFromZero);
+                summary.TotalStampTax = Math.Round(summary.TotalStampTax, 2, MidpointRounding.AwayFromZero);
+                summary.TotalOtherFree = Math.Round(summary.TotalOtherFree, 2, MidpointRounding.AwayFromZero);
+
+                return summary;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+
+        public class MonthSummary
+        {
+            public string Date { get; set; }
+
+            public double MonthShouYi { get; set; }
+
+            public double MonthTradeCount { get; set; }
+
+            public List<Summary> DataList { get; set; }
+        }
+
+        public class Summary
+        {
+            public string Code { get; set; }
+
+            public string Name { get; set; }
+
+            public List<TradeInfo> JiaoGeList { get; set; }
+
+            public double TotalDealAmount { get; set; }    //总成交金额
+
+            public double TotalHappenAmount { get; set; }  //总收益
+
+            public double TotalPoundage { get; set; }      //总手续费
+
+            public double TotalStampTax { get; set; }     //总印花税
+
+            public double TotalOtherFree { get; set; }    //总其他杂费( 过户费 )
+
+            public int TotalCount { get; set; }            //总成交次数
+
+            public int TotalSellCount { get; set; }             //卖出股数
+
+            public int TotalBuyCount { get; set; }              //买入股数
+
+
+        }
 
     }
 }
